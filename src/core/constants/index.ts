5918d8671f03f3d0f02652358b0ff6505ca04eec
Elzocodeur <gningeli03@gmail.com>
2026-02-18 13:34:06 +0000

    deploy

--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -1,5 +1,6 @@
 export const MESSAGES = {
   UNAUTHORIZED: 'Unauthorized access',
   FORBIDDEN: 'Forbidden resource',
-  NOT_FOUND: 'Resource not found'
+  NOT_FOUND: 'Resource not found',
+  INTERNAL_SERVER_ERROR: 'Internal server error',
 };
