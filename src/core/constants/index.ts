049d096610f150271916615f80302a0cacffeda6
Elzocodeur <gningeli03@gmail.com>
2026-02-18 13:40:18 +0000

    deploy test

--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -3,4 +3,6 @@ export const MESSAGES = {
   FORBIDDEN: 'Forbidden resource',
   NOT_FOUND: 'Resource not found',
   INTERNAL_SERVER_ERROR: 'Internal server error',
+  BAD_REQUEST: 'Bad request',
+  UNPROCESSABLE_ENTITY: 'Unprocessable entity',
 };
